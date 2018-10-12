e8058537b0aa012a2cf2ad59e7471da57a17f0ff
xuhuang37 <xuhuang37@outlook.com>
2018-10-12 16:58:24 +0800

    commit(linear):reserve

--- linear/reverse.cpp
+++ linear/reverse.cpp
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
 
-typedef struct LNode{
+typedef struct LNode
+{
     int data;
     struct LNode *next;
-}LNode,*LinkList;
+} LNode, *LinkList;
 
-int reverse(int j,int k, int ary[]){
+int reverse(int j, int k, int ary[])
+{
     int tmp;
-    for(j = j,k = k;j<k;j++,k--){
+    for (j = j, k = k; j < k; j++, k--)
+    {
         tmp = ary[j];
         ary[j] = ary[k];
         ary[k] = tmp;
@@ -17,11 +21,12 @@ int reverse(int j,int k, int ary[]){
     return 0;
 }
 
-
 //q2(1)
-int reverseInM(int left,int right, int ary[], int k){
-    int tmp,i,j;
-    for( i=left ,j=right;i<left+k&&i<j;i++,j--){
+int reverseInM(int left, int right, int ary[], int k)
+{
+    int tmp, i, j;
+    for (i = left, j = right; i < left + k && i < j; i++, j--)
+    {
         tmp = ary[i];
         ary[i] = ary[j];
         ary[j] = tmp;
@@ -29,35 +34,50 @@ int reverseInM(int left,int right, int ary[], int k){
     return 0;
 }
 //q2(2)
-void reverseToEnd(int j,int k, int ary[], int m){
-    reverseInM(j,k,ary,m);
-    reverse(k-m,k,ary);
+void reverseToEnd(int j, int k, int ary[], int m)
+{
+    reverseInM(j, k, ary, m);
+    reverse(k - m, k, ary);
 }
 
 // linklist reverse
 
-void createLinkList(LinkList &L){
-    int n;
-    int l;
-    LNode *tmp,*r = L;
-    std::cin>>n;
-    std::cout<<"&&&&&&&&&&&&&&&&&&&&"<<std::endl;
-    for(int i = 0;i<n;i++){
+void createLinkList(LinkList &L)
+{
+    int n, l;
+    LNode *tmp, *r = L;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         tmp = (LinkList)malloc(sizeof(LNode));
-        std::cout<<"...................."<<std::endl;
-        std::cin>>l;
+        cin >> l;
         tmp->data = l;
         r->next = tmp;
         r = tmp;
     }
 }
 
-void poolLinkList(LinkList &L){
-    std::cout<<"----------------"<<std::endl;
+LNode *poolLinkList(LinkList &L)
+{
     LNode *p = L;
-    while(p->next!=NULL){
-        std::cout<<p->data<<std::endl;
+    cout << "*********************" << endl;
+    while (p->next != NULL)
+    {
         p = p->next;
+        cout << p->data << endl;
+    }
+    cout << p << endl;    
+    return p;
+}
+void reverseLinkList(LinkList &L, LNode *q)
+{
+    LNode *t;
+    while (L->next != q)
+    {
+        t = L->next;
+        L->next = t->next;
+        t->next = q->next;
+        q->next = t;
     }
 }
 
@@ -65,17 +85,19 @@ int main(int argc, char const *argv[])
 {
     // int ary[10];
     // for(int i=0;i<9;i++){
-    //     std::cin>>ary[i];
+    //     cin>>ary[i];
     // }
-    
+
     // reverseToEnd(0,9,ary,1);
     // for(int i = 0;i<9;i++){
-    //     std::cout<<ary[i]<<std::endl;
+    //     cout<<ary[i]<<endl;
     // }
-    LinkList head;
+    LNode *last;
+    LinkList head = (LinkList)malloc(sizeof(LNode));
     createLinkList(head);
+    last = poolLinkList(head);
+    reverseLinkList(head,last);
+    cout<<(head->next)->data<<endl;
     poolLinkList(head);
     return 0;
 }
-
-
--- linner/compr.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    char c = 257;
-    int i = 255;
-    printf("%d ,  %d", c,i);
-    return 0;
-}
