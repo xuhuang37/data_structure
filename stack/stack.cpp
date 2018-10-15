b1135b271d8496b8c64dd1fad9ad44d634663170
xuhuang37 <xuhuang37@outlook.com>
2018-10-15 23:59:26 +0800

    update(stack):stack

---
+++ stack/stack.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+void addStack(int &top,int &){
+
+}
+
+void subStack(int &top,int &){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int stackArray[10];
+
+    return 0;
+}
